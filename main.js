73306cc0e8b5996630fe5da313728507cda7d6b5
trunnghieu <hieuvoduc0107@gmail.com>
2023-11-05 09:19:30 +0700

    Update title and icon

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 onload = () =>{
         document.body.classList.remove("container");
+        playSound();
 };
+
+function playSound() {
+        var audio = new Audio("music.mp3");
+        audio.volume = 0.4;
+        audio.play();
+        if (typeof audio.loop == "boolean") {
+          audio.loop = true;
+        } else {
+          audio.addEventListener(
+            "ended",
+            function () {
+              this.currentTime = 0;
+              this.play();
+            },
+            false
+          );
+        }
+      }
